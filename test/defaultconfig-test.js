b4ea414180b23ec7741857c6e6e95ccb1a85a57f
felipebb <fe.belineb@gmail.com>
2016-06-25 18:31:19 -0300

    + custom ignore tests

--- test/defaultconfig-test.js
+++ test/defaultconfig-test.js
@@ -4,7 +4,8 @@ var assert = require('chai').assert;
 var defaultConfig = require('../lib/default-config');
 
 describe('testing default-config', function () {
-    it('ignore', function () {
+
+    it('ignore csv folder by default', function () {
         var path = '.git';
         var output = defaultConfig.ignore(path);
         assert.isTrue(output);
@@ -12,6 +13,12 @@ describe('testing default-config', function () {
         path = '.git-another';
         output = defaultConfig.ignore(path);
         assert.isNotTrue(output);
+    });
+
+    it('ignore min files by default', function(){
+        var path = 'test.min.js';
+        var output = defaultConfig.ignore(path);
+        assert.isTrue(output);
     });
 
     it('custom ignore path', function () {
@@ -23,10 +30,26 @@ describe('testing default-config', function () {
         output = defaultConfig.ignore(path);
         assert.isTrue(output);
     });
-    
-    it('ignore min files', function(){
-        var path = 'test.min.js';
-        var output = defaultConfig.ignore(path);
+
+    it('custom ignore file name', function() {
+        var file = './kaki.js';
+        var output = defaultConfig.ignore(file);
+        assert.isNotTrue(output);
+
+        defaultConfig.setIgnoreList(null, ['kaki']);
+        output = defaultConfig.ignore(file);
         assert.isTrue(output);
     });
+
+    it('custom ignore extension', function() {
+        var ext = './README.md';
+        var output = defaultConfig.ignore(ext);
+        assert.isNotTrue(output);
+
+        defaultConfig.setIgnoreList(null, null, ['.md']);
+        output = defaultConfig.ignore(ext);
+        assert.isTrue(output);
+    });
+
+
 });
