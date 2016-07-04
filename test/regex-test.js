2f9bd1a34d81afb31e8390f625f14e20fc8572b8
FelipeBB <fe.belineb@gmail.com>
2016-07-03 22:41:41 -0300

    r+ regex tests

--- test/regex-test.js
+++ test/regex-test.js
@@ -11,6 +11,26 @@ describe('Regex helper test', function () {
         output = regex.isRegex(expression);
         expect(output).to.be.true;
     });
+
+    it('successive matches', function () {
+        var input = 'batman nan nan nan';
+        var output = regex.successiveMatches('nan', 'g', input);
+        expect(output).to.be.instanceof(Array);
+        expect(output).to.have.lengthOf(3);
+    });
+
+    it('get regex', function () {
+        var input = '/test[^\s]+/';
+        var output = regex.getRegex(input);
+        expect(output).to.be.equals('test[^\s]+');
+    });
+
+    it('to string', function () {
+        var input = 'lorem} ipsum() $';
+        var output = regex.toString(input);
+        expect(output).to.be.equals('lorem\\} ipsum\\(\\) \\$');
+    });
+
 });
 
 
